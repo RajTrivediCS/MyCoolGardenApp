283a006f8e2aa7f5d2ac7326eb07847550425f30
Noah <59592539+NoahHodgson@users.noreply.github.com>
2020-10-27 11:24:05 -0400

    Add files via upload

---
+++ Garden.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Garden {
+	ArrayList<Plant> gardenElements;
+	String gardenLight;
+	String gardenWater;
+	String gardenSoil;
+	
+	public void addPlant(Plant p, int x, int y) {
+		
+	}
+	public void updatePlantLocation(Plant p, int newX, int newY) {
+		
+	}
+	public void deletePlant(Plant p) {
+		
+	}
+	public int tallyGardenScore() {
+		
+	}
+	public String writeGardenScore() {
+		
+	}
+	public String sumGarden() {
+		
+	}
+}
---
+++ Plant.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Plant {
+	ArrayList<Plant> gardenElements;
+	String gardenLight;
+	String gardenWater;
+	String gardenSoil;
+}
