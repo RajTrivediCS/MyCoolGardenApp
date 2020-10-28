fd008f208bdfa7bd191cc3c570cc2e6e8f6b2414
Noah <59592539+NoahHodgson@users.noreply.github.com>
2020-10-28 13:42:12 -0400

    Update Plant.java

--- Plant.java
+++ Plant.java
@@ -1,8 +1,22 @@
 import java.util.*;
 
 public class Plant {
-	ArrayList<Plant> gardenElements;
+	String name;
+	int xLoc;
+	int yLoc;
 	String gardenLight;
 	String gardenWater;
 	String gardenSoil;
+	
+	Plant(String name, int xLoc, int yLoc){
+		this.name = name;
+		this.xLoc = xLoc;
+		this.yLoc = yLoc;
+		//make another constructor in the future
+	}
+	
+	public void updatePlantLocation(int newX, int newY) {
+		xLoc = newX;
+		yLoc = newY;
+	}
 }
