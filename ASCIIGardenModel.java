5546427ca444b6a8ceb0898d53f61b27136afa41
Noah <59592539+NoahHodgson@users.noreply.github.com>
2020-10-28 22:30:03 -0400

    Added more menu options

--- ASCIIGardenModel.java
+++ ASCIIGardenModel.java
@@ -50,20 +50,23 @@ public class ASCIIGardenModel {
 		//***********************
 		//create bar
 		ArrayList<Plant> hotBarPlants = new ArrayList();
-		hotBarPlants.add(new Plant("autumn bentgrass",0,0));
-		hotBarPlants.add(new Plant("bushy bluestem",0,1));
-		hotBarPlants.add(new Plant("crossvine",0,2));
-		hotBarPlants.add(new Plant("devil's walking stick",0,3));
-		hotBarPlants.add(new Plant("ebony spleenwart",0,4));
-		hotBarPlants.add(new Plant("fern (sensitive)",0,5));
-		hotBarPlants.add(new Plant("giant plumegrass",0,6));
-		hotBarPlants.add(new Plant("heart leaved aster",0,7));
-		hotBarPlants.add(new Plant("white snakeroot",0,8));
-		hotBarPlants.add(new Plant("joe-pye weed",0,9));
-		hotBarPlants.add(new Plant("tall coneflower",0,10));
-		hotBarPlants.add(new Plant("yellow sneezeweed",0,11));
-		hotBarPlants.add(new Plant("red chokeberry",0,12));
-		hotBarPlants.add(new Plant("purple false foxglove",0,13));
+		hotBarPlants.add(new Plant("autumn bentgrass",0,0,"any sun","clay or loamy soil"));
+		hotBarPlants.add(new Plant("bushy bluestem",0,1,"full or partial sun","clay or loamy or sandy soil"));
+		hotBarPlants.add(new Plant("crossvine",0,2,"full or partial sun","clay or loamy or sandy soil"));
+		hotBarPlants.add(new Plant("devil's walking stick",0,3,"full or partial sun","clay or loamy or sandy soil"));
+		hotBarPlants.add(new Plant("ebony spleenwart",0,4,"any sun","clay or loamy or sandy soil"));
+		hotBarPlants.add(new Plant("fern (sensitive)",0,5,"any sun","clay or loamy or sandy soil"));
+		hotBarPlants.add(new Plant("giant plumegrass",0,6,"full or partial sun","loamy or sandy soil"));
+		hotBarPlants.add(new Plant("heart leaved aster",0,7,"partial sun or shade","clay or loamy or sandy soil"));
+		hotBarPlants.add(new Plant("white snakeroot",0,8,"any sun","clay or loamy or sandy soil"));
+		hotBarPlants.add(new Plant("joe-pye weed",0,9,"full or partial sun","sandy soil"));
+		hotBarPlants.add(new Plant("tall coneflower",0,10,"full or partial sun","clay or loamy or sandy soil"));
+		hotBarPlants.add(new Plant("yellow sneezeweed",0,11,"any sun","clay or loamy or sandy soil"));
+		hotBarPlants.add(new Plant("red chokeberry",0,12,"full or partial sun","clay or loamy or sandy soil"));
+		hotBarPlants.add(new Plant("purple false foxglove",0,13,"full sun","sandy soil"));
+		
+		//create empty trash can
+		ArrayList<Plant> trashBin = new ArrayList();
 		
 		//create garden
 		Garden garden = new Garden();
@@ -95,8 +98,8 @@ public class ASCIIGardenModel {
 		System.out.println("Welcome to Team #1's garden application!");
 		//FIXME print garden here
 		Scanner input = new Scanner(System.in);
+		model.printGarden(grid);
 		while(model.running) {
-			model.printGarden(grid);
 			System.out.println("\nEveryting to the right of the double lines is your current garden.");
 			System.out.println("To the left of the double-lines is our hotbar. The lower case letters represents plants "
 					+ "The 'X' represents the trashbin");
@@ -104,99 +107,122 @@ public class ASCIIGardenModel {
 					+ "place the flower");
 			System.out.println("If user inputs where a plant is in the garden he will get information and a prompt asking if he wants to move it");
 			System.out.println("If user moves flower to the capital 'X', plant is deleted. Else, plant is moved");
-			System.out.println("\nInput an x coordinate");
-			int x = input.nextInt();
-			System.out.println("Now input a y coordinate (NOTE: y is going down instead of up)");
-			int y = input.nextInt();
-			
-			//selecting space on the hotbar
-			if(x==0 && y<=13) {
-				Plant tba = new Plant("if you are seeing this you done goofed", 99, 99); // tba == to be added
-				for(Plant p : hotBarPlants) {
-					if(p.yLoc==y) {
-						System.out.println("You have selected: " + p.name + ". Now select where you want to move it");
-						tba=p;
-						break;
-					}
-				}
-				boolean inside = true;
-				while(inside) {
-					System.out.println("Choose an x in your garden: ");
-					int gX = input.nextInt();
-					System.out.println("Good, now choose a y in your garden: ");
-					int gY = input.nextInt();
-					//FIXME: replace with variables in constructor later
-					if(gX>=1 && gX<=9 && gY >=0 && gY <= 14) {
-						garden.addPlant(tba, gX, gY);
-						char z = tba.name.charAt(0);
-						grid[gY][gX] = z;
-						inside=false;
-					}
-					else {
-						System.out.println("Something went wrong. Make sure your x is between 1 and 9 and your y is between 0 and 14");
-					}
-				}
-			}
-			
-			//selecting space in the garden
-			else if(x>0 && x<=9 && y>=0 && y<=14) {
-				boolean plantThere = false;
-				Plant tbm = new Plant("if you see this YOU DONE GOOFED", 99, 99);
-				for(Plant p : garden.gardensPlants) {
-					if(p.xLoc == x && p.yLoc == y) {
-						tbm=p;
-						plantThere=true;
-						System.out.println("You selected " + p.name + " now where do you want it to go. "
-								+ "If you select input 0,14, it will get deleted");
-						
-						//start of while loop
-						boolean inside=true;
-						while(inside) {
-							System.out.println("Select the xLocation you want this plant to go");
-							int gX = input.nextInt();
-							System.out.println("Selecy the y location you want this plant to go");
-							int gY = input.nextInt();
-							if(gX>=1 && gX<=9 && gY >= 0 && gY <= 14) {
-								tbm.updatePlantLocation(gX, gY);
-								grid[y][x] = '*';
-								char z = tbm.name.charAt(0);
-								grid[gY][gX] = z;
-								inside = false;
-							}
-							else if(gX == 0 && gY == 14) {
-								garden.deletePlant(tbm);
-								grid[y][x] = '*';
-								System.out.println("Deletion successful");
-								inside = false;
-							}
-							else {
-								System.out.println("The space you selected is not valid. Try again");
-							}
+			System.out.println("Type one of the following: edit, print, fact, or exit: ");
+			//get command
+			String command = input.next();
+			if(command.equals("edit")) {
+				System.out.println("\nInput an x coordinate");
+				int x = input.nextInt();
+				System.out.println("Now input a y coordinate (NOTE: y is going down instead of up)");
+				int y = input.nextInt();
+				
+				//selecting space on the hotbar
+				if(x==0 && y<=13) {
+					Plant tba = new Plant("if you are seeing this you done goofed", 99, 99); // tba == to be added
+					for(Plant p : hotBarPlants) {
+						if(p.yLoc==y) {
+							System.out.println("You have selected: " + p.name + ". It needs "+ p.plantLight +" and "+ p.plantSoil + 
+									". Now select where you want to move it");
+							tba=p;
+							break;
 						}
-						break;
-						//end of while loop
-						
 					}
-				}
-				if(!plantThere) {
-					System.out.println("You select an area with no plant.");
+					boolean inside = true;
+					while(inside) {
+						System.out.println("Choose an x in your garden: ");
+						int gX = input.nextInt();
+						System.out.println("Good, now choose a y in your garden: ");
+						int gY = input.nextInt();
+						//FIXME: replace with variables in constructor later
+						if(gX>=1 && gX<=9 && gY >=0 && gY <= 14) {
+							garden.addPlant(tba, gX, gY);
+							char z = tba.name.charAt(0);
+							grid[gY][gX] = z;
+							inside=false;
+						}
+						else {
+							System.out.println("Something went wrong. Make sure your x is between 1 and 9 and your y is between 0 and 14");
+						}
+					}
 				}
 				
+				//selecting space in the garden
+				else if(x>0 && x<=9 && y>=0 && y<=14) {
+					boolean plantThere = false;
+					Plant tbm = new Plant("if you see this YOU DONE GOOFED", 99, 99);
+					for(Plant p : garden.gardensPlants) {
+						if(p.xLoc == x && p.yLoc == y) {
+							tbm=p;
+							plantThere=true;
+							System.out.println("You selected a " + p.name + " to be moved. Now where do you want it to go. "
+									+ "If you select input 0,14, it will get deleted");
+							
+							//start of while loop
+							boolean inside=true;
+							while(inside) {
+								System.out.println("Select the xLocation you want this plant to go");
+								int gX = input.nextInt();
+								System.out.println("Selecy the y location you want this plant to go");
+								int gY = input.nextInt();
+								if(gX>=1 && gX<=9 && gY >= 0 && gY <= 14) {
+									tbm.updatePlantLocation(gX, gY);
+									grid[y][x] = '*';
+									char z = tbm.name.charAt(0);
+									grid[gY][gX] = z;
+									inside = false;
+								}
+								else if(gX == 0 && gY == 14) {
+									garden.deletePlant(tbm);
+									grid[y][x] = '*';
+									System.out.println("Deletion successful");
+									inside = false;
+								}
+								else {
+									System.out.println("The space you selected is not valid. Try again");
+								}
+							}
+							break;
+							//end of while loop
+							
+						}
+					}
+					if(!plantThere) {
+						System.out.println("You select an area with no plant.");
+					}
+					
+				}
+				//selecting trash bin
+				else if(x==0 && y==14) {
+					System.out.println("You selected the trashbin. Before selecting this you need to first select "
+							+ "a grid space with in the garden you want to delete");
+				}
+				else {
+					System.out.println("Nice try, but the value you inputed was not within our 15 by 10 grid.");
+				}
 			}
-			//selecting trash bin
-			else if(x==0 && y==14) {
-				System.out.println("You selected the trashbin. Before selecting this you need to first select "
-						+ "a grid space with in the garden you want to delete");
+			else if (command.equals("print")) {
+				model.printGarden(grid);
+			}
+			
+			else if (command.equals("fact")) {
+				System.out.println("Choose the y location of the plant in the left bar that you want to learn about");
+				int yLoc = input.nextInt();
+				for(Plant p : hotBarPlants) {
+					if(p.yLoc==yLoc) {
+						System.out.println("You have selected: " + p.name + ". It needs "+ p.plantLight +" and "+ p.plantSoil+".");
+					}
+				}
+			}
+			else if(command.equals("exit")) {
+				System.out.println("Exit program?(yes/no)");
+				String exit = input.next();
+				if(exit.equals("yes")) {
+					model.running = false;
+				}
 			}
 			else {
-				System.out.println("Nice try, but the value you inputed was not within our 15 by 10 grid.");
-			}
-			System.out.println("Exit?(yes/no)");
-			String exit = input.next();
-			if(exit.equals("yes")) {
-				model.running = false;
+				System.out.println("Nice try buddy but that wasn't a choice.");
 			}
 		}
-		
 	}
 }
