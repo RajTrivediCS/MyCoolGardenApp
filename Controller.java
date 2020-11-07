088b5d9dcece9c2f2afe90c6d36e3d14633da9f0
RajTrivediCS <triudrav@udel.edu>
2020-11-06 21:13:26 -0500

    View mimicking Lab 8 without Drag and Drop...

--- Controller.java
+++ Controller.java
@@ -1,10 +1,20 @@
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
-public class Controller {
-	Model model = new Model();
-	View view = new View();
-	
-	//functions go here
-	public void main(String[]args) {
-		
+public class Controller extends Application {
+
+	View view;
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		view = new View(stage);
 	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 }
