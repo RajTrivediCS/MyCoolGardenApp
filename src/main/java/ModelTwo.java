9c2ce27b1b4d82ab5c8c95588d03d2bd197fd09a
Noah Hodgson <noahmhodgson@gmail.com>
2020-12-09 12:16:49 -0500

    reading through a csv

--- src/main/java/ModelTwo.java
+++ src/main/java/ModelTwo.java
@@ -24,7 +24,7 @@ public class ModelTwo {
 		this.garden = new Garden();
 		this.trashBin = new ArrayList<>();
 		try {
-			this.hotBarPlants = getPlantsListFromFile("plants.txt");
+			this.hotBarPlants = getPlantsListFromFile("plants.csv");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -41,7 +41,7 @@ public class ModelTwo {
 		ArrayList<Plant> plantsList = new ArrayList<>();
 		while(sc.hasNextLine()) {
 			String currLine = sc.nextLine();
-			String[] parts = currLine.split("-");
+			String[] parts = currLine.split(";");
 			plantsList.add(new Plant(parts[NAMESPOT],0,0,parts[SUNSPOT],parts[SOILSPOT],parts[SIZESPOT]));
 		}
 		return plantsList;
